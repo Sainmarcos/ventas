789830ada42289880e61fa2553f26c2c9ac57762
Marcos Sain <sain.marcos@gmail.com>
2022-11-18 17:30:26 -0300

    Rename main

--- src/main/java/com/mycompany/tiendabesysoft/Main.java
+++ src/main/java/com/mycompany/tiendabesysoft/Main.java
@@ -1,7 +1,7 @@
 package com.mycompany.tiendabesysoft;
 
 
-public class TiendaBesysoft {
+public class Main {
 
     public static void main(String[] args) {
         Tienda miTienda = new Tienda();
